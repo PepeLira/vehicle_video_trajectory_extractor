b1230232f40ef916377b697cf79e0a5ea487b3a0
Jose Edwards <jflira@miuandes.cl>
2023-10-22 22:05:47 -0300

    feat(video model): new model for video interaction

---
+++ desktop_aplication/src/models/input_video.py
@@ -0,0 +1,40 @@
+import cv2
+
+class InputVideo:
+    def __init__(self, video_path):
+        self.video_path = video_path
+
+    def get_video_path(self):
+        return self.video_path
+    
+    def get_frames(self):
+        video = cv2.VideoCapture(self.video_path)
+        while True:
+            ret, frame = video.read()
+            if ret:
+                yield frame
+            else:
+                break
+        video.release()
+
+    def get_video_fps(self):
+        video = cv2.VideoCapture(self.video_path)
+        fps = video.get(cv2.CAP_PROP_FPS)
+        video.release()
+        return fps
+
+    def display_frames(self, frame):
+        for frame in self.get_frames():
+            cv2.imshow("Frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    def __str__(self):
+        return self.video_path
+        
+
+if __name__ == "__main__":
+    input_video = InputVideo("../../../videos/video1_30s_sift_estabilizado_filtrado.mp4")
+    frame = input_video.get_frames()
+    input_video.display_frames(frame)
+    
